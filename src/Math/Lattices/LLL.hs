47acf2acf13a5745c131389bb6c213d341233596
Bart Coppens <kde@bartcoppens.be>
2011-10-16 00:10:24 +0200

    Implementation of main LLL Algorithm

--- src/Math/Lattices/LLL.hs
+++ src/Math/Lattices/LLL.hs
@@ -9,12 +9,15 @@ import           Data.Ratio
 import           Math.Algebra.LinearAlgebra     hiding ((!))
 import           Math.LinearAlgebra.GramSchmidt
 
+type Basis = Array Int [Rational]
+type GSO   = Array (Int, Int) Rational
+
 -- The $B_i$ set is called 'bb in this file, because of course we cannot call it 'B in Haskell.
 
 -- TODO ### use STUArray instead of lists, AAAARG silly Haskell
 
 -- | Just an easy way to write $||v||^2$
-norm2 v = v <*> v
+norm2 v = v <.> v
 
 -- | Closest 'Integral to the given n, rounding down (TODO: up?). $\lfloor n\rceil$
 -- rnd = round
@@ -23,12 +26,20 @@ norm2 v = v <*> v
 lll basis = lllDelta basis $ 3%4
 
 -- | Return an LLL reduced basis, with reduction parameter $\delta$. This is the conventional flavor of the algorithm using Gram-Schmidt, no fancy speedups yet
-lllDelta basis delta = lll' basis delta gsB mu bi 1
+lllDelta basis delta = lllLoop b' delta bb' mu' 1 n
     where
-        (gsB, mu) = gramSchmidtOrthogonalization basis
-        bi        = map norm2 gsB
+        n       = length basis - 1
+        (b, mu) = gramSchmidtOrthogonalization basis
+        bb      = map norm2 b
+
+        b'      = listArray (0, n) b
+        bb'     = listArray (0, n) bb
+        mu'     = listArray ( (0, 0), (n, n) ) [ m | i     <- [0..n],
+                                                     j     <- [0..n],
+                                                     let m =  (mu !! i) !! j ] -- TODO
 
 -- | Perform a size reduction. Returns the new $b_k$, the new $\mu_k$.
+sizeReduction :: Int -> Basis -> GSO -> (Basis, GSO)
 sizeReduction k b mu = sizeReduction' indices k b mu
     where
         indices = reverse $ [0..k-2]
@@ -53,6 +64,7 @@ sizeReduction'' k l b mu = (b', mu'')
         mu'' = mu' // [ ( (k, l), mu' ! (k, l) - r) ]
 
 -- | Returns whether the Lovász Condition holds: $B_k \geq \delta - \mu^2_{k,k-1}B_{k-1}$
+lovaszCondition :: Array Int Rational -> Int -> Rational -> GSO -> Bool
 lovaszCondition bb k delta mu = (bb ! k) >= (delta - m^2)*(bb ! (k-1))
     where
         m = mu ! (k, k-1)
@@ -79,10 +91,15 @@ swapBaseVectors b bb mu k = (b, bb', mu'')
                     update_i_k1 i = (mu' ! (k, k-1)) * (mu' ! (i, k-1)) + (mu' ! (i, k)) - m*(mu' ! (i, k)) * (mu' ! (k, k-1))
                     update_i_k  i = (mu' ! (i, k-1)) - m * (mu' ! (i, k))
 
-lll' = undefined
-
 -- | The main loop of the LLL algorithm. We reduce basis 'b with $\delta$ 'delta, with a Gram-Schmidt basis $b^*$ as 'b and the $\mu_{i,j}$ coefficients in 'mu.
 --   The current iteration of the loop is 'k out of a maximum of 'n
---lllLoop b delta b mu k n | lovaszCondition bb k delta mu = lllLoop ... $ min 1 $ k - 1
-                         -- | otherwise                     = lllLoop basis delta gsB mu bi $ k + 1
-        -- (b', mu') = sizeReduction b'
+lllLoop :: Basis -> Rational -> Array Int Rational -> GSO -> Int -> Int -> Basis
+lllLoop b delta bb mu k n | k == n    = b
+                          | isLovasz  = lllLoop b'  delta bb  mu'  (k+1) n
+                          | otherwise = lllLoop b'' delta bb' mu'' nextk n
+    where
+        (b', mu')        = sizeReduction k b mu
+        isLovasz         = lovaszCondition bb k delta mu'
+
+        (b'', bb', mu'') = swapBaseVectors b' bb mu' k
+        nextk            = min 1 $ k - 1

97f703ad04b82726e6bb3726b80046d5e02012c9
Bart Coppens <kde@bartcoppens.be>
2012-02-24 18:22:21 +0100

    I'll be implementing the floating point version, too

---
+++ src/Math/Lattices/LLLFP.hs
@@ -0,0 +1,11 @@
+-- | Implements a LLL lattice reduction algorithm with floating point arithmetic. References for this algorithm:
+--
+--   * Lattice Basis Reduction: Improved Practical Algorithms and Solving Subset Sum Problems, C. P. Schorr and M. Euchner (1993)
+--
+module Math.Lattices.LLLFP (
+    lllFP,
+    lllFPDelta
+) where
+
+lllFP = undefined
+lllFPDelta = undefined
